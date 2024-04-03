c1b557ca3d1f7e861888e4a45cf99747683a9464
Bruno Mazzoni <85739942+BrunoMazzoni2023@users.noreply.github.com>
2024-04-02 21:10:20 -0300

    primeira entidade

---
+++ src/main/java/com/bruno/mazzoni/MazzoniApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.mazzoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazzoniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazzoniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/mazzoni/MazzoniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.mazzoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazzoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
